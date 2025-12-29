1cb874bd1d6fe855e08cd65d1d61f1c2486c2ecd
RzkyPra <151447760+YeOldeTard@users.noreply.github.com>
2025-12-30 01:01:03 +0700

    Update db.py
    Signed-off-by: RzkyPra <151447760+YeOldeTard@users.noreply.github.com>

--- db.py
+++ db.py
@@ -2,9 +2,9 @@ import pymysql
 
 def getdb():
     return pymysql.connect(
-        host="localhost",
-        user="root",
-        password="",
+        host="easyfood.mysql.database.azure.com",
+        user="admin_jawa",
+        password="admin_jawa1",
         database="easyfood_db",
         cursorclass=pymysql.cursors.DictCursor
     )
