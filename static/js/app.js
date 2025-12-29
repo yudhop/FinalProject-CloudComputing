b5fc650b5ade74a01782cbef28cb18c27374a651
RzkyPra <151447760+YeOldeTard@users.noreply.github.com>
2025-12-30 01:33:40 +0700

    Update app.js
    Signed-off-by: RzkyPra <151447760+YeOldeTard@users.noreply.github.com>

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,6 @@
 // APP JAVASCRIPT
 
-const API_URL = "client-easyfood-anhrasg7d6a2azb9.indonesiacentral-01.azurewebsites.net;
+const API_URL = "client-easyfood-anhrasg7d6a2azb9.indonesiacentral-01.azurewebsites.net";
 const contentFeed = document.getElementById("contentFeed");
 const searchInput = document.getElementById("searchInput");
 const sidebarItems = document.querySelectorAll(".sidebar-nav-menu .nav-item");
@@ -769,4 +769,5 @@ sidebarItems.forEach(item=>{
 // START
 checkLoginSession();
 initApp();
+
 
